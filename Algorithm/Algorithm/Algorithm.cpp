fa514c1ef0739a0fe14d1bd5cf8293501e3f16f3
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2022-03-04 21:40:09 +0900

    add 1932

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -6,8 +6,9 @@
 
 using namespace std;
 
-long long dp[91][3];
-long long sum = 0;
+long long dp[501][501];
+long long tree[501][501];
+long long result = 0;
 
 int main()
 {
@@ -17,21 +18,43 @@ int main()
 	int n;
 	cin >> n;
 
-	dp[1][0] = 1;
-	dp[1][1] = 0;
-	dp[2][0] = 1;
-	dp[2][1] = 0;
-
-	for (int i = 3; i <= n; i++)
+	for (int i = 1; i <= n; i++)
 	{
-		dp[i][0] = dp[i - 1][0] + dp[i - 1][1];
-		dp[i][1] = dp[i - 1][0];
-	}
-	
-	for (int i = 0; i <= 1; i++)
-	{
-		sum += dp[n][i];
+		for (int j = 1; j <= i; j++)
+		{
+			cin >> tree[i][j];
+		}
 	}
 
-	cout << sum;
+	dp[1][1] = tree[1][1];
+
+
+	for (int i = 2; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			if (j == 1)
+			{
+				dp[i][j] = dp[i - 1][j] + tree[i][j];
+			}
+			else if (j == i)
+			{
+				dp[i][j] = dp[i - 1][j - 1] + tree[i][j];
+			}
+			else
+			{
+				dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + tree[i][j];
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (dp[n][i] > result)
+		{
+			result = dp[n][i];
+		}
+	}
+
+	cout << result;
 }
---
+++ DP/1932.cpp
@@ -0,0 +1,60 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <stack>
+#include <string>
+
+using namespace std;
+
+long long dp[501][501];
+long long tree[501][501];
+long long result = 0;
+
+int main()
+{
+	cin.tie(NULL);
+	ios::sync_with_stdio(false);
+
+	int n;
+	cin >> n;
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			cin >> tree[i][j];
+		}
+	}
+
+	dp[1][1] = tree[1][1];
+
+
+	for (int i = 2; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			if (j == 1)
+			{
+				dp[i][j] = dp[i - 1][j] + tree[i][j];
+			}
+			else if (j == i)
+			{
+				dp[i][j] = dp[i - 1][j - 1] + tree[i][j];
+			}
+			else
+			{
+				dp[i][j] = max(dp[i - 1][j - 1], dp[i - 1][j]) + tree[i][j];
+			}
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		if (dp[n][i] > result)
+		{
+			result = dp[n][i];
+		}
+	}
+
+	cout << result;
+}
