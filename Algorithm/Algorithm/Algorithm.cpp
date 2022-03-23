feefedc712a4e429beee1bddab2c59238e64c764
Nomadzz <82443328+Camelllia@users.noreply.github.com>
2022-03-24 02:33:37 +0900

    add 3055

--- Algorithm/Algorithm/Algorithm.cpp
+++ Algorithm/Algorithm/Algorithm.cpp
@@ -1,70 +1,131 @@
 ﻿#include <iostream>
 #include <algorithm>
 #include <queue>
-#include <stack>
-#include <cstring>
+#define x first
+#define y second
 
 using namespace std;
 
-const int MAX = 100005;
-int visited[MAX];
-int parent[MAX];
-int n, k;
-queue<pair<int, int>> q;
-stack<int> st;
+int n, m, waterCnt;
+char board[51][51];
+int distWater[51][51];
+int distBeaver[51][51];
+bool visited[51][51];
+int caveX, caveY;
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+queue<pair<int, int>> waterQ;
+queue<pair<int, int>> beaverQ;
 
 int main()
 {
 	cin.tie(0);
 	ios::sync_with_stdio(0);
 
-	cin >> n >> k;
+	cin >> n >> m;
 
-	q.push({n, 0});
-	visited[n] = 1;
-
-	while (!q.empty())
+	for (int i = 0; i <= n; i++)
 	{
-		int pos = q.front().first;
-		int sec = q.front().second;
-		q.pop();
-
-		if (pos == k)
+		for (int j = 0; j <= m; j++)
 		{
-			cout << sec << "\n";
-			int now = k;
-			while (now != n)
-			{
-				st.push(now);
-				now = parent[now];
-			}
-			st.push(n);
+			distWater[i][j] = -1;
+			distBeaver[i][j] = -1;
+			visited[i][j] = false;
 		}
+	}
 
-		for (int dir : {pos + 1, pos - 1, pos * 2})
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
 		{
-			if (dir < 0 || dir >= MAX)
+			char c;
+			cin >> c;
+			board[i][j] = c;
+
+			if (c == '*')
+			{
+				waterQ.push({ i,j });
+				distWater[i][j] = 0;
+				visited[i][j] = true;
+			}
+			if (c == 'S')
+			{
+				beaverQ.push({ i,j });
+				distBeaver[i][j] = 0;
+			}
+			if (c == 'D')
+			{
+				caveX = i;
+				caveY = j;
+			}
+		}
+	}
+
+	while (!waterQ.empty())
+	{
+		auto cur = waterQ.front();
+		waterQ.pop();
+		
+		for (int dir = 0; dir < 4; dir++)
+		{
+			int nx = cur.x + dx[dir];
+			int ny = cur.y + dy[dir];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m)
 			{
 				continue;
 			}
 			else
 			{
-				if (!visited[dir])
+				if (board[nx][ny] != 'X' && distWater[nx][ny] == -1 && board[nx][ny] != 'D' && board[nx][ny] != '*')
 				{
-					visited[dir] = 1;
-					parent[dir] = pos;
-					q.push({ dir, sec + 1 });
+					distWater[nx][ny] = distWater[cur.x][cur.y] + 1;
+					waterQ.push({ nx, ny });
+					visited[nx][ny] = true;
 				}
-				
-			}	
+			}
 		}
 	}
 
-	while (!st.empty())
+	while (!beaverQ.empty())
 	{
-		cout << st.top() << " ";
-		st.pop();
+		auto cur = beaverQ.front();
+		beaverQ.pop();
+
+		for (int dir = 0; dir < 4; dir++)
+		{
+			int nx = cur.x + dx[dir];
+			int ny = cur.y + dy[dir];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+			{
+				continue;
+			}
+			else
+			{
+				if (board[nx][ny] == 'D')
+				{
+					distBeaver[nx][ny] = distBeaver[cur.x][cur.y] + 1;
+					cout << distBeaver[nx][ny];
+					return 0;
+				}
+				if (visited[nx][ny] == false && distBeaver[nx][ny] == -1 && board[nx][ny] != 'X')
+				{
+					distBeaver[nx][ny] = distBeaver[cur.x][cur.y] + 1;
+					beaverQ.push({ nx, ny });
+					continue;
+				}
+				if (board[nx][ny] != 'X' && distBeaver[nx][ny] == -1 && distBeaver[cur.x][cur.y] + 1 < distWater[nx][ny])
+				{
+					distBeaver[nx][ny] = distBeaver[cur.x][cur.y] + 1;
+					beaverQ.push({ nx, ny });
+				}
+			}
+
+		}
 	}
+
+	cout << "KAKTUS";
 }
 
 
---
+++ DFS, BFS/3055.cpp
@@ -0,0 +1,131 @@
+#include <iostream>
+#include <algorithm>
+#include <queue>
+#define x first
+#define y second
+
+using namespace std;
+
+int n, m, waterCnt;
+char board[51][51];
+int distWater[51][51];
+int distBeaver[51][51];
+bool visited[51][51];
+int caveX, caveY;
+int dx[4] = { 1,0,-1,0 };
+int dy[4] = { 0,1,0,-1 };
+queue<pair<int, int>> waterQ;
+queue<pair<int, int>> beaverQ;
+
+int main()
+{
+	cin.tie(0);
+	ios::sync_with_stdio(0);
+
+	cin >> n >> m;
+
+	for (int i = 0; i <= n; i++)
+	{
+		for (int j = 0; j <= m; j++)
+		{
+			distWater[i][j] = -1;
+			distBeaver[i][j] = -1;
+			visited[i][j] = false;
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		for (int j = 0; j < m; j++)
+		{
+			char c;
+			cin >> c;
+			board[i][j] = c;
+
+			if (c == '*')
+			{
+				waterQ.push({ i,j });
+				distWater[i][j] = 0;
+				visited[i][j] = true;
+			}
+			if (c == 'S')
+			{
+				beaverQ.push({ i,j });
+				distBeaver[i][j] = 0;
+			}
+			if (c == 'D')
+			{
+				caveX = i;
+				caveY = j;
+			}
+		}
+	}
+
+	while (!waterQ.empty())
+	{
+		auto cur = waterQ.front();
+		waterQ.pop();
+
+		for (int dir = 0; dir < 4; dir++)
+		{
+			int nx = cur.x + dx[dir];
+			int ny = cur.y + dy[dir];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+			{
+				continue;
+			}
+			else
+			{
+				if (board[nx][ny] != 'X' && distWater[nx][ny] == -1 && board[nx][ny] != 'D' && board[nx][ny] != '*')
+				{
+					distWater[nx][ny] = distWater[cur.x][cur.y] + 1;
+					waterQ.push({ nx, ny });
+					visited[nx][ny] = true;
+				}
+			}
+		}
+	}
+
+	while (!beaverQ.empty())
+	{
+		auto cur = beaverQ.front();
+		beaverQ.pop();
+
+		for (int dir = 0; dir < 4; dir++)
+		{
+			int nx = cur.x + dx[dir];
+			int ny = cur.y + dy[dir];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m)
+			{
+				continue;
+			}
+			else
+			{
+				if (board[nx][ny] == 'D')
+				{
+					distBeaver[nx][ny] = distBeaver[cur.x][cur.y] + 1;
+					cout << distBeaver[nx][ny];
+					return 0;
+				}
+				if (visited[nx][ny] == false && distBeaver[nx][ny] == -1 && board[nx][ny] != 'X')
+				{
+					distBeaver[nx][ny] = distBeaver[cur.x][cur.y] + 1;
+					beaverQ.push({ nx, ny });
+					continue;
+				}
+				if (board[nx][ny] != 'X' && distBeaver[nx][ny] == -1 && distBeaver[cur.x][cur.y] + 1 < distWater[nx][ny])
+				{
+					distBeaver[nx][ny] = distBeaver[cur.x][cur.y] + 1;
+					beaverQ.push({ nx, ny });
+				}
+			}
+
+		}
+	}
+
+	cout << "KAKTUS";
+}
+
+
